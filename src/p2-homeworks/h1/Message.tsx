33ffcd31e82175855b63ea4262df3b91ee08fa9c
arturkudli <arturkapes@gmail.com>
2021-06-06 22:43:33 +0300

    start work with h/w 1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import {messageData} from "./HW1";
 
-function Message() {
+type PropsType = {
+    name: string
+    message: string
+    avatar: string
+    time: string
+}
+
+function Message(props: PropsType) {
     return (
         <div>
-
+            <img src={messageData.avatar}/>
+            {messageData.name}
+            {messageData.message}
+            {messageData.time}
         </div>
     )
 }
-
 
 export default Message
